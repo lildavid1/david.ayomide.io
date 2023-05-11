a6c3a18981e6be13b2973f758616921f8784ee73
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-05-11 15:53:53 +0100

    Update and rename project/requirements.txt to build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+pip install gunicorn
+pip install flask
+pip install cs50
+pip install flask_session
+pip install flask_mail
+pip install werkzeug
+pip install tempfile
+python -m pip install --upgrade pip
