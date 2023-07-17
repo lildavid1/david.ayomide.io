7f9fc323d103e692999ca425adbf8feb581da540
lildavid1 <lild93078@gmail.com>
2023-07-17 15:42:53 +0000

    Done confimation of email

--- project/build.sh
+++ project/build.sh
@@ -1,10 +1,10 @@
+python -m pip install --upgrade pip
 pip install gunicorn
 pip install flask
 pip install cs50
 pip install flask_session
 pip install flask_mail
 pip install werkzeug
-python -m pip install --upgrade pip
 pip install psycopg2-binary
 pip install psycopg2
 pip install mysqlclient
@@ -13,4 +13,4 @@ pip install secrets
 pip install redis
 export FLASK_ENV="development"
 export FLASK_DEBUG=1
-pip install random string
+pip install random
