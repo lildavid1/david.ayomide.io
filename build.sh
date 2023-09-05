ba834556df1930bd4382cfa11aacfb00954a465a
Youngdavid1 <116070327+Youngdavid1@users.noreply.github.com>
2023-09-05 09:19:51 +0000

    Tue, Sep 5, 2023, 10:19 AM +01:00

--- build.sh
+++
@@ -1,8 +0,0 @@
-pip install gunicorn
-pip install flask
-pip install cs50
-pip install flask_session
-pip install flask_mail
-pip install werkzeug
-pip install tempfile
-python -m pip install --upgrade pip
