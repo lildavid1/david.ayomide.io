82051f176034fd74a247c70d14b0211d9bdea26f
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-10-18 15:36:25 +0000

    Wed, Oct 18, 2023, 8:36 AM -07:00

--- project/index.js
+++ project/index.js
@@ -1,9 +1,10 @@
 const planetscale = require('@planetscale/database');
 
 const connection = planetscale.connect({
-  host: 'aws.connect.psdb.cloud', // e.g., aws.connect.psdb.cloud
-  user: 'bysidvltse7rfxg9yq2f', // e.g., wtb3duvh5dy4n67qrr48
-  password: 'pscale_pw_ykn7ikOpzq3bcTFbe8awBu3AI48vXjZewJ9gVkS3mEo', // e.g., your-password
+  host: 'aws.connect.psdb.cloud',
+  user: 'bysidvltse7rfxg9yq2f',
+  password: 'pscale_pw_ykn7ikOpzq3bcTFbe8awBu3AI48vXjZewJ9gVkS3mEo',
+  database: 'adebayo-shopping'
 });
 
 console.log(connection.execute("SELECT * FROM registrants"))
