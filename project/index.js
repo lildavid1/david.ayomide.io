6fa38a026c305f101759e38503c6436916536047
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-10-18 15:22:29 +0000

    Wed, Oct 18, 2023, 8:22 AM -07:00

--- project/index.js
+++ project/index.js
@@ -1,15 +1,1 @@
-const planetscale = require('@planetscale/database');
 
-console.log(planetscale)
-planetscale.connect({
-  host: 'aws.connect.psdb.cloud',
-  username: 'wtb3duvh5dy4n67qrr48',
-  password: 'pscale_pw_UoxvB9rrC1LgHXZjDmgDzCYdO7n4SSgPvBewd78PNDv'
-});
-
-const results =  planetscale.execute('select 1 from dual where 1=?', [1])
-console.log(results)
-
-async function tryko(){
-  console.log(await planetscale)
-}
