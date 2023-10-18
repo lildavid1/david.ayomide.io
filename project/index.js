e54b03ef5e661247ac8b1e794d355a23c5dba736
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-10-18 15:47:25 +0000

    Wed, Oct 18, 2023, 8:47 AM -07:00

--- project/index.js
+++ project/index.js
@@ -18,15 +18,7 @@ async function runQuery() {
     password: 'pscale_pw_ykn7ikOpzq3bcTFbe8awBu3AI48vXjZewJ9gVkS3mEo',
     database: 'adebayo-shopping'
   });
-
-  try {
-    const results = await connection.execute("SELECT * FROM registrants");
-    console.log(results);
-  } catch (error) {
-    console.error('Error executing the query:', error);
-  } finally {
-    connection.end(); // Always close the connection
-  }
-}
+  console.log(await connection.execute("SELECT * FROM registrants"))
+};
 
 runQuery();
