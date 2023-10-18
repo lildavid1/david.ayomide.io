acad1d460f823f12b20fa9c98d66db853d9922c6
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-10-18 15:56:34 +0000

    Wed, Oct 18, 2023, 8:56 AM -07:00

--- project/static/js/script.js
+++ project/static/js/script.js
@@ -1,5 +1,18 @@
 'use strict';
+const planetscale = require('@planetscale/database');
 
+async function runQuery() {
+  const connection = planetscale.connect({
+    host: 'aws.connect.psdb.cloud',
+    user: 'bysidvltse7rfxg9yq2f',
+    password: 'pscale_pw_ykn7ikOpzq3bcTFbe8awBu3AI48vXjZewJ9gVkS3mEo',
+    database: 'adebayo-shopping'
+  });
+  // console.log(connection.execute("SELECT count(*) FROM registrants"))
+  console.log(connection.execute())
+};
+
+runQuery();
 //selectors here
 const input = document.querySelector('.kol');
 const btns = document.querySelectorAll('.btn');
