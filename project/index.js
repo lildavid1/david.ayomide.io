faef270722df07a42197e1b17439960c9f465db2
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-10-18 15:25:57 +0000

    Wed, Oct 18, 2023, 8:25 AM -07:00

--- project/index.js
+++ project/index.js
@@ -1,1 +1,8 @@
+const planetscale = require('@planetscale/database');
+
+const connection = planetscale.createConnection({
+  host: 'your-database-host', // e.g., aws.connect.psdb.cloud
+  user: 'your-database-username', // e.g., wtb3duvh5dy4n67qrr48
+  password: 'your-database-password', // e.g., your-password
+});
 
