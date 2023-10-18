838401875dc53ad604244d735ff8cbc7024a5fc5
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-10-18 15:39:59 +0000

    Wed, Oct 18, 2023, 8:39 AM -07:00

--- project/index.js
+++ project/index.js
@@ -1,10 +1,32 @@
+// const planetscale = require('@planetscale/database');
+
+// const connection = planetscale.connect({
+//   host: 'aws.connect.psdb.cloud',
+//   user: 'bysidvltse7rfxg9yq2f',
+//   password: 'pscale_pw_ykn7ikOpzq3bcTFbe8awBu3AI48vXjZewJ9gVkS3mEo',
+//   database: 'adebayo-shopping'
+// });
+
+// console.log(connection.execute("SELECT * FROM registrants"))
+
 const planetscale = require('@planetscale/database');
 
-const connection = planetscale.connect({
-  host: 'aws.connect.psdb.cloud',
-  user: 'bysidvltse7rfxg9yq2f',
-  password: 'pscale_pw_ykn7ikOpzq3bcTFbe8awBu3AI48vXjZewJ9gVkS3mEo',
-  database: 'adebayo-shopping'
-});
+async function runQuery() {
+  const connection = planetscale.connect({
+    host: 'aws.connect.psdb.cloud',
+    user: 'bysidvltse7rfxg9yq2f',
+    password: 'pscale_pw_ykn7ikOpzq3bcTFbe8awBu3AI48vXjZewJ9gVkS3mEo',
+    database: 'adebayo-shopping'
+  });
 
-console.log(connection.execute("SELECT * FROM registrants"))
+  try {
+    const results = await connection.execute("SELECT * FROM registrants");
+    console.log(results);
+  } catch (error) {
+    console.error('Error executing the query:', error);
+  } finally {
+    connection.end(); // Always close the connection
+  }
+}
+
+runQuery();
