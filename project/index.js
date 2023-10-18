01882fa3a3fc056f43f5f5b57e7ec14c567a4502
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-10-18 14:48:47 +0000

    Wed, Oct 18, 2023, 7:48 AM -07:00

--- project/index.js
+++ project/index.js
@@ -1,30 +1,31 @@
 const planetscale = require('@planetscale/database');
 
 // Define your database connection settings
-const connection = planetscale.createConnection({
-  host: 'aws.connect.psdb.cloud',
-  user: 'your-mysql-username',
-  password: 'your-mysql-password',
-  database: 'your-mysql-database',
-});
+// const connection = planetscale.createConnection({
+//   host: 'aws.connect.psdb.cloud',
+//   user: 'your-mysql-username',
+//   password: 'your-mysql-password',
+//   database: 'your-mysql-database',
+// });
 
-// Connect to the database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
-  console.log('Connected to the database');
+// // Connect to the database
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to the database:', err);
+//     return;
+//   }
+//   console.log('Connected to the database');
 
-  // Perform a query
-  connection.query('SELECT * FROM your_table', (queryError, results) => {
-    if (queryError) {
-      console.error('Error executing the query:', queryError);
-    } else {
-      console.log('Query results:', results);
-    }
+//   // Perform a query
+//   connection.query('SELECT * FROM your_table', (queryError, results) => {
+//     if (queryError) {
+//       console.error('Error executing the query:', queryError);
+//     } else {
+//       console.log('Query results:', results);
+//     }
 
-    // Close the connection
-    connection.end();
-  });
-});
+//     // Close the connection
+//     connection.end();
+//   });
+// });
+console.log(planetscale)
