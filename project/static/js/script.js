505e6fc7a5b51deeb97a812ebdc57f5a003b5db4
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-10-23 15:05:40 +0100

    Delete some lines of code

--- project/static/js/script.js
+++ project/static/js/script.js
@@ -1,18 +1,5 @@
 'use strict';
-const planetscale = require('@planetscale/database');
 
-async function runQuery() {
-  const connection = planetscale.connect({
-    host: 'aws.connect.psdb.cloud',
-    user: 'bysidvltse7rfxg9yq2f',
-    password: 'pscale_pw_ykn7ikOpzq3bcTFbe8awBu3AI48vXjZewJ9gVkS3mEo',
-    database: 'adebayo-shopping'
-  });
-  // console.log(connection.execute("SELECT count(*) FROM registrants"))
-  console.log(connection.execute())
-};
-
-runQuery();
 //selectors here
 const input = document.querySelector('.kol');
 const btns = document.querySelectorAll('.btn');
@@ -40,7 +27,6 @@ btns?.forEach((...btn)=>{
 });
 
 
-// console.log(productPrice?.__proto__);
 productPrice?.forEach(function(price){
     console.log(Number(price.innerText));
 
