ad620ad097086f18790e064dc7a9c49f6a06715e
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-11-01 15:47:35 +0000

    Wed, Nov 1, 2023, 8:47 AM -07:00

--- project/static/js/script.js
+++ project/static/js/script.js
@@ -81,9 +81,9 @@ const upperFirstWord = function(str){
 }
 
 const transformer = function(str, fn){
-    console.log(`Original string: ${fn}`)
+    console.log(`Original string: ${str}`)
     console.log(`Transformed string ${fn(str)}`)
     console.log(`Transformed by: ${fn}`)
 }
 
-transformer('Javascript is the best!', upperFirstWord())
+transformer('Javascript is the best!', upperFirstWord)
