07b6ac6549852f84ac9c1708f35cea3b4a5df914
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-11-03 14:20:52 +0000

    Fri, Nov 3, 2023, 7:20 AM -07:00

--- project/application.py
+++ project/application.py
@@ -194,7 +194,7 @@ def index():
 
 @app.route("/api/user")
 def api():
-    userirequest.args.get("userid"))
+    userid = request.args.get("userid")
     row = dbp.execute("SELECT * FROM registrants") if userid == 'users' else dbp.execute("SELECT * FROM registrants WHERE username = ?", userid)
     return row
 
