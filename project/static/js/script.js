79a60597a949d193b8aad8eda742c9ab0a2ca45f
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-11-27 10:07:27 +0100

    New Codespace

--- project/static/js/script.js
+++ project/static/js/script.js
@@ -54,9 +54,6 @@ plusAll?.forEach((c, i) => {
         console.log(+quantitySize[i].textContent++)
     })
 })
-const jumiaApi = async function() {
-    const response = await fetch('https://www.jumia.com.ng/fashion-smart-fashion-breathable-unisex-sneakerscanvas-white-43411455.html')
-    console.log(await response.text())
-}
+
 
 
