385b6238683237601721afabb329709512fc84bc
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-12-01 08:23:14 +0000

    Fri, Dec 1, 2023, 9:23 AM +01:00

--- project/static/js/script.js
+++ project/static/js/script.js
@@ -12,7 +12,7 @@ const minusAll = document.querySelectorAll('.minus')
 const plusAll = document.querySelectorAll('.plus')
 const quantitySize = document.querySelectorAll('.quantity')
 
-input?.addEventListener('key', async function(e) {
+input?.addEventListener('keypress', async function(e) {
     let response = await fetch(`/search?q=${this.value}`);
     let searchList = await response.json();
     let html = '';
