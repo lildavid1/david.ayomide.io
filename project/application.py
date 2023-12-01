836713ac2c9ce674b3b0d2afbab547f9624b72a8
lildavid1 <110206867+lildavid1@users.noreply.github.com>
2023-12-01 10:01:15 +0000

    Fri, Dec 1, 2023, 11:01 AM +01:00

--- project/application.py
+++ project/application.py
@@ -171,7 +171,8 @@ def search():
 
 @app.route("/view/<a>")
 def product_view(a):
-    products = dbl.execute("SELECT * FROM products WHERE title = (?)", a)
+    print(a.replace('/', ' '))
+    products = dbl.execute("SELECT * FROM products WHERE title = (?)", a.replace('/', ' '))
     return render_template("product_view.html", products=products)
 
 
